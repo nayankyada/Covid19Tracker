08deae3fc78a91814c7eeb594e8b608468139ee6
nayankyada <kayadanayan@gmail.com>
2021-02-15 17:43:38 +0530

    1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,46 @@
+import { FormControl, MenuItem, Select } from "@material-ui/core";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 export default function App() {
+  const [countries, setCountries] = useState(["USA", "India"]);
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const counries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl>
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country.value}>{country.name}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+
+      {/* infobox */}
+      {/* infobox */}
+      {/* infobox */}
+
+      {/* Table */}
+      {/* Graph */}
+
+      {/* Map */}
     </div>
   );
 }
